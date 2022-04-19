be4225a142b0231863a129fb021d8aabec824696
bassem eloraby <BASEM12683@GMAIL.COM>
2022-04-19 17:34:11 +0300

    Revert "routes"
    This reverts commit 03142a79fe3a0efc3796c648bdcfba9bda2dc79c.

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
 const express = require('express');
-const mongoose = require('mongoose');
-const app = express();
-// const req = require('express/lib/request');
-// const { render } = require('express/lib/response');
-// const res = require('express/lib/response');
+const req = require('express/lib/request');
+const { render } = require('express/lib/response');
+const res = require('express/lib/response');
 const methodOverride = require('method-override');
 const Task = require('./models/tasks');
-const router = require('./routes/tasks');
-
+const mongoose = require('mongoose');
+const app = express();
 
 app.use(methodOverride('_method',{methods:['POST','GET']}));
 
@@ -21,6 +19,18 @@ mongoose.connect('mongodb://localhost:27017/ToDo', {
 });
 
 
-app.use('/', router);
+
+//insert //localhost:3000/create/:title
+// app.post('/create',);
+//find//show
+// app.get('/',);
+
+//delete //localhost:3000/delete/:id
+// app.delete('/delete/:id',);
+
+//update
+// app.get('/update/:id',);
+
+// app.put("/update/:id",)
 
 app.listen(3000, () => console.log('express has started!'));
--- models/tasks.js
+++ models/tasks.js
@@ -1,10 +1,9 @@
 const mongoose = require('mongoose');
 
 const schema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true
-  }
-});
-
+     title: {
+        type: String,
+        required: true
+    } 
+    });
 module.exports = mongoose.model('Task', schema);
